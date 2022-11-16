12f8e06732f80634807e19e9eee50d2891492359
robin mahmud <robinmahmud358@gmail.com>
2022-11-16 21:13:16 +0600

    deshboard

--- index.js
+++ index.js
@@ -33,6 +33,15 @@ async function run() {
             })
             res.send(Options)
         })
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email
+            const query = { email: email }
+            const bookings = await bookingsCollection.find(query).toArray()
+            res.send(bookings)
+        })
+
+
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             console.log(booking)
